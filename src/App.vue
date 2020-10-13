4c04d8779b46ce8e301658d14051b3ee4d6b93ea
Jose Guerra <jrguerra.180490@gmail.com>
2020-10-13 19:17:27 -0300

    Agregando transicion

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,11 @@
         </div>
     </nav>
     <!-- ROUTER VIEW QUE CARGARÁ LAS VISTAS (INICIO, SOBRE MÍ, CONTACTO, ETC.) -->
-    <router-view></router-view> 
+    <transition name="v8vue"> 
+      <router-view>
+
+      </router-view>
+     </transition>
   </div>
   
 </template>
@@ -50,5 +54,14 @@ export default {
 }
 #menu-v8 a{
   text-shadow: 1px 1px #000;
+}
+
+.v8vue-enter-active,
+.v8vue-leave-active{
+  transition: opacity 1s;
+}
+.v8vue-enter,
+.v8vue-leave-to {
+opacity: 0;
 }
 </style>
